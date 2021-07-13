7143089742e2c228172e70a4942615002ae645e2
Rafael <rvfael@bk.ru>
2021-07-13 15:26:54 +0500

    дз - 3 (финальная)

--- js/API.js
+++ js/API.js
@@ -3,19 +3,6 @@ import dataHandler from "./dataHandler.js"
 
 export default {
     fetch() {
-        return dataHandler.getCatalog().then((data) => data);
+        return dataHandler.getCatalog();
     }
 }
-
-
-
-// fetch() {
-//     return [
-//         { title: "ELLERY X M'O CAPSULE", description: 'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.', price: 52.00, img1: 'img/product_1.webp', img2: 'img/product_1.jpg' },
-//         { title: "ELLERY X M'O CAPSULE", description: 'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.', price: 52.00, img1: 'img/product_2.webp', img2: 'img/product_2.jpg' },
-//         { title: "ELLERY X M'O CAPSULE", description: 'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.', price: 52.00, img1: 'img/product_3.webp', img2: 'img/product_3.jpg' },
-//         { title: "ELLERY X M'O CAPSULE", description: 'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.', price: 52.00, img1: 'img/product_4.webp', img2: 'img/product_4.jpg' },
-//         { title: "ELLERY X M'O CAPSULE", description: 'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.', price: 52.00, img1: 'img/product_5.webp', img2: 'img/product_5.jpg' },
-//         { title: "ELLERY X M'O CAPSULE", description: 'Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.', price: 52.00, img1: 'img/product_6.webp', img2: 'img/product_6.jpg' }
-//     ]
-// }
--- js/main.js
+++ js/main.js
@@ -7,33 +7,28 @@ import GoodsList from "./model/GoodsList.js";
 import GoodCart from "./model/GoodCart.js";
 
 
-
-function addToCart(id) {
-    const good = goodList.getById(id);
-    cart.add(new GoodCart(good));
-    const icon = document.querySelector('#cart-bt');
-    icon.insertAdjacentHTML('beforeend', `<span>${cart.getQuantity()}</span>`)
-}
-
-
-console.log(API.fetch()); //Выводится промис
-
 API.fetch().then((data) => {
-    console.log(data);  //выводится то что нужно но если поставить return функция все равно возвращает промис.
+    const goodList = new GoodsList(data);
+
+    const $product__cards = document.querySelector('.product__cards');
+
+    const cart = new Cart([]);
+
+    const cards = goodList.get().map(good => new Card(good));
+
+
+    cards.forEach(card => {
+        card.render($product__cards);
+        card.setAddHandler(addToCart);
+    });
+
+    function addToCart(id) {
+        const good = goodList.getById(id);
+        cart.add(new GoodCart(good));
+        const icon = document.querySelector('#cart-bt');
+        icon.insertAdjacentHTML('beforeend', `<span>${cart.getQuantity()}</span>`)
+    }
 })
 
-const goodList = new GoodsList(API.fetch());
-
-const cart = new Cart([]);
-
-const $product__cards = document.querySelector('.product__cards');
-
-const cards = goodList.get().map(good => new Card(good));
-
-
-cards.forEach(card => {
-    card.render($product__cards);
-    card.setAddHandler(addToCart);
-});
 
 
